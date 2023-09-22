18a5c946d8b875b1dbfb465c356c999aad033e93
Caio <caio-cesarlima2010@hotmail.com>
2023-09-21 21:43:47 -0300

    commit1

---
+++ Exercicio1/Exercicio1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+namespace Curso
+{
+    public class Retangulo
+
+    {
+        public double Largura;
+        public double Altura;
+        public double area()
+        {
+            double areaCalc = Altura * Largura;
+            return areaCalc; 
+    }
+        public double perimetro()
+        {
+            double perimetroCalc = 2*(Altura + Largura);
+            return perimetroCalc;
+        }
+        public double diagonal()
+        {
+            double diagonalCalc = Math.Sqrt(Largura*Altura + Altura*Altura);
+            return diagonalCalc;
+        }
+    }
+}
---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Curso;
+using System;
+using System.Formats.Asn1;
+
+namespace programa 
+{ 
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Retangulo RetanguloEntrada = new Retangulo();
+            Console.Write("Digite a Altura do Retangulo: ");
+            RetanguloEntrada.Altura = double.Parse(Console.ReadLine());
+            Console.Write("Digite a Largura/Base do Retangulo: ");
+            RetanguloEntrada.Largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual operação deseja realizar?");
+            Console.WriteLine("1-Area do Retângulo");
+            Console.WriteLine("2-Perimetro do Retângulo");
+            Console.WriteLine("3-Diagonal do Retângulo");
+            string resposta = Console.ReadLine();
+
+            if (resposta == "1")
+            {
+                Console.WriteLine($"A Área do Retângulo é igual à: {RetanguloEntrada.area().ToString()}");
+            }
+            else if (resposta == "2")
+            {
+                Console.WriteLine($"A Área do Retângulo é igual à: {RetanguloEntrada.perimetro().ToString()}");
+            }
+            else 
+            {
+                Console.WriteLine($"A Área do Retângulo é igual à: {RetanguloEntrada.diagonal().ToString()}");
+            }
+
+        }
+    }
+}
