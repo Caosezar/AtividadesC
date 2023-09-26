cd994e62a8eeac42ca81b32ad00ec3f51cb4be89
Caio_lima <115504991+Caosezar@users.noreply.github.com>
2023-09-25 21:14:45 -0300

    CalculadoraWithStaticMethods

---
+++ CalculadoraMetodoStatico/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Course 
+{
+    class Program 
+    { 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine($"Circunferência: {circ}");
+            Console.WriteLine($"Volume: {volume}");
+            Console.WriteLine($"Valor de Pi: {Calculadora.Pi}");
+        }
+    }
+}
